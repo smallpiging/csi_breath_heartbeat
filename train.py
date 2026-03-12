c371da02a654a60ca32919df2c3fc7fae04bd253
smallpiging <2714007082@qq.com>
2026-03-12 15:01:48 +0800

    test_loader

--- train.py
+++ train.py
@@ -3,26 +3,33 @@ import torch.nn as nn
 import torch.optim as optim
 from unet import UNet1D
 from datasets import CSIDataset
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 from matplotlib import pyplot as plt
 
 def train():
     # 参数
     batch_size = 4
     learning_rate = 0.0001
-    epochs = 50
+    epochs = 100
 
     # 准备设备
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
 
     # 准备数据集
-    train_Dataset = CSIDataset(data_dir="./processed_datasets", window_size=256, step=20)
-    train_loader = DataLoader(dataset=train_Dataset, batch_size=batch_size, shuffle=True)
-    print(f'数据集加载成功，{len(train_Dataset)}个样本,分为{len(train_loader)}个批次')
+    full_dataset = CSIDataset(data_dir="./processed_datasets", window_size=256, step=20)
+    # 按 8:2 的比例计算分界线
+    dataset_size = len(full_dataset)
+    train_size = int(0.8 * dataset_size)
+    test_size = dataset_size - train_size
+    train_dataset = Subset(full_dataset, range(0, train_size))
+    test_dataset = Subset(full_dataset, range(train_size, dataset_size))
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    print(f"✅ 数据加载完毕！总样本: {dataset_size} | 训练集: {train_size} | 测试集: {test_size}")
 
     # 准备模型
-    model = UNet1D(num_encoding_blocks=3, in_channels=52, out_classes=1, out_channels_first_layer=64, preactivation=True, residual=True).to(device)
+    model = UNet1D(num_encoding_blocks=3, in_channels=52, out_classes=1, out_channels_first_layer=128, preactivation=True, residual=True).to(device)
     total_params = sum(p.numel() for p in model.parameters())
     print('模型参数：',total_params)
 
@@ -45,17 +52,29 @@ def train():
             optimizer.step()
 
             epoch_loss += loss.item()
+            avg_train_loss = epoch_loss / len(train_loader)
             # if (batch_idx + 1) % 10 == 0:
             #     print(f"Epoch [{epoch + 1}/{epochs}] | Batch [{batch_idx + 1}/{len(train_loader)}] | 当前 Loss: {loss.item():.4f}")
 
-        print(f'epoch:{epoch}, epoch_loss: {epoch_loss:.4f}')
+
+            model.eval()  # 极其关键：关闭 Dropout 和 BatchNorm 的动态更新，进入闭卷考试模式
+            val_loss_accum = 0.0
+            with torch.no_grad():  # 极其关键：考试时不计算梯度，省显存提速度
+                for x_val, y_val in test_loader:
+                    x_val, y_val = x_val.to(device), y_val.to(device)
+                    val_prediction = model(x_val)
+                    val_loss = criterion(val_prediction, y_val)
+                    val_loss_accum += val_loss.item()
+
+            avg_val_loss = val_loss_accum / len(test_loader)
+        print(f'epoch:{epoch}, avg_train_loss: {avg_train_loss:.4f}, avg_val_loss: {avg_val_loss:.4f}')
     # ================= 激动人心的预测与可视化环节 =================
     print("\n🎨 开始绘制预测结果...")
     model.eval()  # 极其关键：告诉大厨现在是考试，不要再修改脑细胞了
 
     # 1. 抽查第一个样本 (注意解包 x 和 真实的 y)
     # (假设你的 Dataset 变量名叫 train_dataset，如果你用的是 train_Dataset 请对应修改)
-    x_sample, y_true_sample = train_Dataset[40]
+    x_sample, y_true_sample = full_dataset[47]
 
     # 2. 升维与上显卡
     # 把 [52, 256] 变成 [1, 52, 256]，并送进 GPU
